52dff3f18a1837a39bc1350c13f69d1187956cc3
flynn81 <tom.karam@gmail.com>
2021-02-27 02:01:36 -0500

    Adding some more log statements

--- model/cockroach_user.go
+++ model/cockroach_user.go
@@ -17,7 +17,7 @@ type CockroachUserAccess struct {
 func (ua CockroachUserAccess) Get(email string) (*User, error) {
 	rows, err := db.Query("select id, name, email, hash, status from task_user where email = $1", email)
 	if err != nil {
-		zap.S().Infow("%e", err)
+		zap.S().Infof("%e", err)
 		return nil, err
 	}
 	defer closeRows(rows)
@@ -28,7 +28,7 @@ func (ua CockroachUserAccess) Get(email string) (*User, error) {
 	if rows.Next() {
 		err := rows.Scan(&i, &n, &e, &h, &s)
 		if err != nil {
-			zap.S().Infow("%e", err)
+			zap.S().Infof("%e", err)
 			return nil, err
 		}
 		return &User{i, n, e, h, s}, nil
@@ -41,17 +41,17 @@ func (ua CockroachUserAccess) Create(u *User) *User {
 	stmt, err := db.Prepare("INSERT INTO TASK_USER (id, name, email, status) VALUES ($1, $2, $3, $4)")
 
 	if err != nil {
-		zap.S().Infow("%e", err)
+		zap.S().Infof("%e", err)
 		return nil
 	}
 	_, err = stmt.Exec(uuid.NewString(), u.Name, u.Email, u.Status)
 	if err != nil {
-		zap.S().Infow("%e", err)
+		zap.S().Infof("%e", err)
 		return nil
 	}
 	createdUser, err := ua.Get(u.Email)
 	if err != nil {
-		zap.S().Infow("%e", err)
+		zap.S().Infof("%e", err)
 		return nil
 	}
 	return createdUser
@@ -61,17 +61,17 @@ func (ua CockroachUserAccess) Create(u *User) *User {
 func (ua CockroachUserAccess) UpdatePassword(i string, p string) bool {
 	stmt, err := db.Prepare("UPDATE TASK_USER SET hash=$1 WHERE id = $2")
 	if err != nil {
-		zap.S().Infow("%e", err)
+		zap.S().Infof("%e", err)
 		return false
 	}
 	h, err := bcrypt.GenerateFromPassword([]byte(p), 14)
 	if err != nil {
-		zap.S().Infow("%e", err)
+		zap.S().Infof("%e", err)
 		return false
 	}
 	_, err = stmt.Exec(h, i)
 	if err != nil {
-		zap.S().Infow("%e", err)
+		zap.S().Infof("%e", err)
 		return false
 	}
 	return true
